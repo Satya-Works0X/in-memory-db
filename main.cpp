651d6ce30fbe9d185bea1770a5a4ccbc16bb5df1
Satya-Works0X <boyrksat@gmail.com>
2025-12-29 23:08:38 +0530

    new_udpate_02

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    // A record consists of a key and a value stored in memory.
+    // DB= HashMap<Key, Value>
+
+    unordered_map<string, string> db;
+    string command;
+    string key, value;
+    string report;
+    // DB part
+
+    while (cin >> command)
+    {
+        if (command == "INSERT")
+        {
+            cin >> key >> value;
+            db[key] = value;
+        }
+        else if (command == "GET")
+        {
+            cin >> key;
+            if (db.find(key) != db.end())
+            {
+                cout << db[key] << endl;
+            }
+            else
+            {
+                cout << "Key Not Found" << endl;
+            }
+        }
+        else if (command == "UPDATE")
+        {
+            cin >> key >> value;
+            if (db.find(key) != db.end())
+            {
+                db[key] = value;
+            }
+            else
+            {
+                cout << "Key Not Found" << endl;
+            }
+        }
+        else if (command == "DELETE")
+        {
+            cin >> key;
+            if (db.find(key) != db.end())
+            {
+                db.erase(key);
+            }
+            else
+            {
+                cout << "Key Not Found" << endl;
+            }
+        }
+        else if (command == "EXIT")
+        {
+            return 0;
+        }
+    }
+
+    return 0;
+}
